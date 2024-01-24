4ba81ed04500eb4e150f8c0abf9cfe69a467d802
nikthe883 <popmihaylov@gmail.com>
2024-01-24 15:55:09 +0200

    commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./header.css"
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+import HeaderSocial from './HeaderSocials'
+
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I am</h5>
+        <h1>Peter</h1>
+        <h5 className='text-light'>Junior Python Developer</h5>
+        <CTA></CTA>
+        <HeaderSocial></HeaderSocial>
+
+        <div className="me">
+          <img src={ME} alt='me'></img>
+        </div>
+
+        <a href='#contact' className='scroll__down'>Scroll Down</a>
+
+      </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+
+const HeaderSocials  = () => {
+  return (
+    <div className='header__socials'>
+        <a href='https://www.linkedin.com/in/peterpopmihaylov/' target='_blank'><FaLinkedinIn /></a>
+        <a href='https://github.com/nikthe883' target='_blank'><FaGithub /></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
