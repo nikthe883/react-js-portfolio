ba69167cd2abfb5e3b2a055e6fd3448edad833bb
nikthe883 <popmihaylov@gmail.com>
2024-01-25 13:26:32 +0200

    commit

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -17,20 +17,29 @@ const About = () => {
         <div className='about__content'>
           <div className="about__cards">
             <article className='about__card'>
-              <h5>Experiance</h5>
-              <small>3 years of Experiance</small>
+            <h4>Experiance</h4>
+              <p>3 years of Experiance</p>
             </article>
 
             <article className='about__card'>
-              <h5>Experiance</h5>
-              <small>3 years of Experiance</small>
+              <h4>Experiance</h4>
+              <p>3 years of Experiance</p>
             </article>
 
             <article className='about__card'>
-              <h5>Experiance</h5>
-              <small>3 years of Experiance</small>
+            <h4>Experiance</h4>
+              <p>3 years of Experiance</p>
             </article>
+            
           </div>
+          <div>
+              <p>some text some text some text some text some text some text some textsome 
+                text some textsome textsome textsome textsome textsome 
+                some textsome textsome textsome textsome textsome text
+                some textsome textsome textsome textsome text
+                some textsome textsome textsome texttextsome textsome textsome text</p>
+            </div>
+            <a href='#contact' className='btn btn-primary'> Lets Talk!</a>
         </div>
       </div>
     </section>
