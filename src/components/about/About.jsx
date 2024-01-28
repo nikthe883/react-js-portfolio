a96ed540c2c32ad0c747ad073fb034ca7af3d546
nikthe883 <popmihaylov@gmail.com>
2024-01-28 20:41:21 +0200

    more bug fixes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import { useState } from 'react'
-import "./about.css"
-import ME from '../../assets/me-about.jpg'
+import React from 'react';
+import { useState } from 'react';
+import "./about.css";
+import ME from '../../assets/me-about.jpg';
 
 const data = [
-  { title: "Experiance", experiance: "3 years of Experiance" },
-  { title: "Python", experiance: "Advanced" },
-  { title: "Python", experiance: "Advanced" },
+  {title: "Experience", experiance: "3 years of Experience", content: '1 content' },
+  {title: "Python", experiance: "Advanced", content: '2 content' },
+  {title: "React", experiance: "Intermediate", content: '3 content' },
 ];
 
-
-
 const About = () => {
-  const [showMore, setShowMore] = useState(false)
-  const hancleClick = () => {
-    setShowMore(!showMore)
+  const [showMore, setShowMore] = useState(Array(data.length).fill(false));
+
+  const handleClick = (index) => {
+    const newShowMore = [...showMore];
+    newShowMore[index] = !newShowMore[index];
+    setShowMore(newShowMore);
   };
 
   return (
@@ -24,44 +25,40 @@ const About = () => {
       <div className='container about__container'>
         <div className='about__me'>
           <div className='about__me-image'>
-            <img src={ME} alt='About Image'/>
+            <img src={ME} alt='About Image' />
           </div>
-
         </div>
-        
 
-          <div className='about__content'>
-            <div className="about__cards">
-
-           
-                {data.map(({title, experiance }) => {
-          return (
-            <article className='about__card'>
-            <h4>{title}</h4>
-              <p>{experiance}</p>
-              <a className='btn' onClick={hancleClick}>Show More</a>
-              {showMore &&(
-                <h2>More content</h2>
-              )}
-            </article>
-          );
-        })}
-                
+        <div className='about__content'>
+          <div className="about__cards">
+            {data.map(({ id, title, experiance, content }, index) => (
+              <article key={id} className='about__card'>
+                <h4>{title}</h4>
+                <p>{experiance}</p>
+                <a className='btn' onClick={() => handleClick(index)}>
+                  {showMore[index] ? 'Show Less' : 'Show More'}
+                </a>
+                {showMore[index] && (
+                  <h2>{content}</h2>
+                )}
+              </article>
+            ))}
           </div>
+
           <div>
-              <p className='about__me__p'>Some text some text some text some text some text some text some textsome 
-                text some textsome textsome textsome textsome textsome 
-                some textsome textsome textsome textsome textsome text
-                some textsome textsome textsome textsome text
-                some textsome textsome textsome texttextsome textsome textsome text</p>
-            </div >
-            <div className='lets__talk'>
+            <p className='about__me__p'>Some text some text some text some text some text some text some textsome
+              text some textsome textsome textsome textsome textsome
+              some textsome textsome textsome textsome textsome text
+              some textsome textsome textsome textsome text
+              some textsome textsome textsome texttextsome textsome textsome text</p>
+          </div>
+          <div className='lets__talk'>
             <a href='#contact' className='btn btn-primary'> Lets Talk!</a>
-            </div>
+          </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
