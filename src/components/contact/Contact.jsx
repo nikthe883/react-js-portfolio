e99a8069b4a05941d1039ab6df00f87fcb441f1b
nikthe883 <popmihaylov@gmail.com>
2024-02-01 16:34:03 +0200

    more work done

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,18 @@ import "./contact.css"
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h3>Get in Touch</h3>
+      <h1>Contact Me</h1>
+    <div className='container contact__container'>
+    <form action=''>
+      <input type='text' name='name' placeholder='Your Full Name' required />
+      <input type='email' name='email'placeholder='Your Email' required />
+      <textarea name='message' rows="7" placeholder='Enter Your Message' required></textarea>
+      <button type='submit' className='btn btn-primary'>Send Message</button>
+    </form>
+    </div>
+    </section>
   )
 }
 
