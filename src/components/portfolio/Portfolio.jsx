c76c02b2320bfdef5c91f3311b450ae90e68acf8
nikthe883 <popmihaylov@gmail.com>
2024-02-01 14:35:34 +0200

    more changes

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
+import { useState } from 'react';
 import "./portfolio.css"
 import IMG1 from '../../assets/portfolio1.jpg'
 
 
 const data = [
-  {title: "Portfolio item title 1", img: IMG1, github:"https://github.com/", content:"Show more 1", demo:"https://github.com/" },
+  {title: "Portfolio item title 1", img: IMG1, github:"https://github.com/nikthe883/Django-Thought-Project", content:"Show more 1", demo:"https://github.com/" },
   {title: "Portfolio item title 2", img: IMG1, github:"https://github.com/", content:"Show more 2", demo:"https://github.com/" },
   {title: "Portfolio item title 3", img: IMG1, github:"https://github.com/", content:"Show more 3", demo:"https://github.com/" },
   {title: "Portfolio item title 4", img: IMG1, github:"https://github.com/", content:"Show more 4", demo:"https://github.com/" },
 ];
 
 const Portfolio = () => {
+
+  const [showMore, setShowMore] = useState(Array(data.length).fill(false));
+
+  const handleClick = (index) => {
+    const newShowMore = [...showMore];
+    newShowMore[index] = !newShowMore[index];
+    setShowMore(newShowMore);
+  };
+
   return (
     <section id='portfolio'>
     <h5> My Recent Work </h5>
@@ -18,75 +28,26 @@ const Portfolio = () => {
 
     <div className='container portfolio__container'>
 
-      <article className='portfolio__item'>
-      <h2>This is portfolio item title</h2>
-        <div className='portfolio__item-image'>
-          <img src={IMG1}></img>
-          </div>
-          <div className='portfolio__links'>
+      {data.map(({ id, title, img, github, content, demo }, index) => (
+              <article key={id} className='portfolio__item'>
+                <h2>{title}</h2>
+                <img src={img}></img>
+                <div className='portfolio__links'>
 
-          <a href='github.com' className='btn' target='_blank'>Github</a>
-          <a href='' className='btn long-text' target='_blank'>Show More</a>
-          <a href='' className='btn long-text' target='_blank'>Live Demo</a>
+                <a href={github} className='btn' target='_blank'>Github</a>
 
-          <a href='' className='btn short-text' target='_blank'>More</a>
-          <a href='' className='btn short-text' target='_blank'>Demo</a>
-          </div>
-          
-        
-      </article>
-      <article className='portfolio__item'>
-      <h3>This is portfolio item title</h3>
-        <div className='portfolio__item-image'>
-          <img src={IMG1}></img>
-          </div>
-          <div className='portfolio__links'>
-          <a href='github.com' className='btn' target='_blank'>Github</a>
-          <a href='' className='btn' target='_blank'>Show More</a>
-          <a href='' className='btn' target='_blank'>Live Demo</a>
-          </div>
-          
-        
-      </article>
-      <article className='portfolio__item'>
-      <h3>This is portfolio item title</h3>
-        <div className='portfolio__item-image'>
-          <img src={IMG1}></img>
-          </div>
-          <div className='portfolio__links'>
-          <a href='github.com' className='btn' target='_blank'>Github</a>
-          <a href='' className='btn' target='_blank'>Show More</a>
-          <a href='' className='btn' target='_blank'>Live Demo</a>
-          </div>
-          
-        
-      </article>
-      <article  className='portfolio__item'>
-      <h3>This is portfolio item title</h3>
-        <div  className='portfolio__item-image'>
-          <img src={IMG1}></img>
-          </div>
-          <div className='portfolio__links'>
-          <a href='github.com' className='btn' target='_blank'>Github</a>
-          <a href='' className='btn' target='_blank'>Show More</a>
-          <a href='' className='btn' target='_blank'>Live Demo</a>
-          </div>
-          
-        
-      </article>
-      <article  className='portfolio__item'>
-      <h3>This is portfolio item title</h3>
-        <div className='portfolio__item-image'>
-          <img src={IMG1}></img>
-          </div>
-          <div className='portfolio__links'>
-          <a href='github.com' className='btn' target='_blank'>Github</a>
-          <a href='' className='btn' target='_blank'>Show More</a>
-          <a href='' className='btn' target='_blank'>Live Demo</a>
-          </div>
-          
-        
-      </article>
+                <a className='btn long-text' onClick={() => handleClick(index)}>
+                  {showMore[index] ? 'Show Less' : 'Show More'}
+                </a>
+                {showMore[index] && (
+                  <h2>{content}</h2>
+                )}
+
+                <a href={demo} className='btn long-text' target='_blank'>Live Demo</a>
+
+                </div>
+              </article>
+            ))}
      
 
     </div>
