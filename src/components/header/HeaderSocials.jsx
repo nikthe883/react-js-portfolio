b44d833aa10edaa6325fd88941aaffeb969c563c
nikthe883 <popmihaylov@gmail.com>
2024-02-28 20:15:57 +0200

    fixed a bug with links not working

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,7 +5,7 @@ import { FaGithub } from "react-icons/fa";
 const HeaderSocials  = () => {
   return (
     <div className='header__socials'>
-        <a href='https://www.linkedin.com/in/peterpopmihaylov/' target='_blank'><FaLinkedinIn /></a>
+        <a href="https://www.linkedin.com/in/peterpopmihaylov/" target='_blank'><FaLinkedinIn /></a>
         <a href='https://github.com/nikthe883' target='_blank'><FaGithub /></a>
     </div>
   )
