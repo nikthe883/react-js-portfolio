03196bde14fb3c8522e9d5e19903880d11eb187c
nikthe883 <popmihaylov@gmail.com>
2024-04-23 20:53:21 +0300

    code clean up

--- src/components/header/MostRecentHeader.jsx
+++ src/components/header/MostRecentHeader.jsx
@@ -5,19 +5,8 @@ import "./highlight.css"
 import IMG1 from '../../assets/portfolio2.jpg'
 
 
-const data = [
-  {title: "ShopyThing", img: IMG1, github:"https://github.com/nikthe883/Django-Thought-Project", content:"Show more 111333331111", demo:"https://github.com/" },
-];
-
 const Highlight = () => {
 
-  const [showMore, setShowMore] = useState(Array(data.length).fill(false));
-
-  const handleClick = (index) => {
-    const newShowMore = [...showMore];
-    newShowMore[index] = !newShowMore[index];
-    setShowMore(newShowMore);
-  };
 
   return (
     <section id='highlight'>
@@ -25,22 +14,16 @@ const Highlight = () => {
     <h1> ShopyThing </h1>
 
     <div className='container highlight__container'>
-
-      {data.map(({ id, title, img, github, content, demo }, index) => (
-              <article key={id} className='highlight__item'>
-                <h2>{title}</h2>
-                <img className='highlight__image' src={img}></img>
-                <div className='highlight__links'>
-                <Link className='btn long-text' spy to="portfolio">Go to Project</Link>
-                </div>
-              </article>
-            ))}
-     
-
+        <article className='highlight__item'>
+            <h2>ShopyThing</h2>
+            <img className='highlight__image' src={IMG1}></img>
+            <div className='highlight__links'>
+            <Link className='btn long-text' spy to="portfolio">Go to Project</Link>
+            </div>
+        </article>
     </div>
     
     </section>
-    
   )
 }
 
