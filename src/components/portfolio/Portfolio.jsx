e74bccb73af76a16a710f6a9670b995e45cea8d7
nikthe883 <popmihaylov@gmail.com>
2024-04-26 22:47:17 +0300

    update

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { useState } from 'react';
 import "./portfolio.css"
-import IMG1 from '../../assets/portfolio2.jpg'
+import IMG1 from '../../assets/shoppy.PNG'
+import IMG2 from '../../assets/thought.png'
+import IMG3 from '../../assets/huff.PNG'
+import IMG4 from '../../assets/wolf.PNG'
 
 
 const data = [
   {title: "Portfolio item title 1", img: IMG1, github:"https://github.com/nikthe883/Django-Thought-Project", content:"Show more 111333331111", demo:"https://github.com/" },
-  {title: "Portfolio item title 2", img: IMG1, github:"https://github.com/", content:"Show more 2223333333222", demo:"https://github.com/" },
-  {title: "Portfolio item title 3", img: IMG1, github:"https://github.com/", content:"Show more 32333332222", demo:"https://github.com/" },
-  {title: "Portfolio item title 4", img: IMG1, github:"https://github.com/", content:"Show more 423333332222", demo:"https://github.com/" },
+  {title: "Portfolio item title 2", img: IMG2, github:"https://github.com/", content:"Show more 2223333333222", demo:"https://github.com/" },
+  {title: "Portfolio item title 3", img: IMG3, github:"https://github.com/", content:"Show more 32333332222", demo:"https://github.com/" },
+  {title: "Portfolio item title 4", img: IMG4, github:"https://github.com/", content:"Show more 423333332222", demo:"https://github.com/" },
 ];
 
 const Portfolio = () => {
